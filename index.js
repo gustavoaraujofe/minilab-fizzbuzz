594a291147780ba4e2bc037cd0156d7bae1ecebe
Gustavo Araujo <gustavoaraujofe@gmail.com>
2021-10-18 18:57:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-// Seu código aqui!
+for(let i = 1; i <= 100; i++){
 
-let count = 1;
-
-while (count <= 100) {
-  if (i % 3 === 0) {
-    console.log("fizz");
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log('Fizz Buzz');
+  } else if(i % 3 === 0){
+    console.log('Fizz');
+  } else if(i % 5 === 0){
+    console.log('Buzz')
+  } else {
+    console.log(i);
   }
-
-  count++;
 }
